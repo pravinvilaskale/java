31ada762727137ecc2c032cc4e732fcf350fbfed
pravinvilaskale <pravinvkale889@gmail.com>
2022-04-21 17:19:04 +0530

    Hello World

--- C2TC/src/First/Hello.java
+++ C2TC/src/First/Hello.java
@@ -1,10 +1,10 @@
 package First;
 
-public class Solution {
+public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello.");
+
 	}
 
 }
